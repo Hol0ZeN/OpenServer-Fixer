cab93ea274b6f705aaa9dc938d3e4c0f617a87f5
Hol0ZeN <42004187+Hol0ZeN@users.noreply.github.com>
2019-04-25 18:05:55 +0300

    Release

---
+++ OpenServer-Fixer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace OpenServer_Fixer
+{
+    class Program
+    {
+        private static FileSystemWatcher Watcher = new FileSystemWatcher();
+
+        private static void Main(string[] args)
+        {
+            string directory = string.Empty;
+            do
+            {
+                Console.Write("Path to database module directory: ");
+                directory = Console.ReadLine();
+                if (Directory.Exists(directory)) break;
+                Console.WriteLine("Wrong directory!");
+            } while (!Directory.Exists(directory));
+
+            Watcher.Path = directory;
+            Watcher.Filter = "my.ini";
+            Watcher.NotifyFilter = NotifyFilters.LastWrite;
+            Watcher.EnableRaisingEvents = true;
+            Watcher.Changed += Watcher_Changed;
+            Console.WriteLine("Ready for OpenServer start!");
+            Console.ReadKey();
+        }
+
+        private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string text = File.ReadAllText(e.FullPath);
+            File.WriteAllText(e.FullPath, text, new ASCIIEncoding());
+        }
+    }
+}
